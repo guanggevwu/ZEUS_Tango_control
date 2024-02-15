a70f8bb0ce59cd778d6897d9ff62cba3124554d4
Qing Zhang <guanggevwu@gmail.com>
2024-02-15 15:26:10 -0500

    add a general tarus form client GUI

---
+++ taurus_form.py
@@ -0,0 +1,22 @@
+import sys
+import taurus
+from taurus.qt.qtgui.panel import TaurusForm
+from taurus.external.qt import Qt
+# from taurus.qt.qtgui import extra_guiqwt
+from taurus.qt.qtgui.application import TaurusApplication
+import tango
+
+app = TaurusApplication(sys.argv, cmd_line_parser=None)
+w = TaurusForm()
+
+device_name = 'test/basler/1'
+dp = tango.DeviceProxy(device_name)
+# dp = taurus.Device(device_name)
+
+attrs = dp.get_attribute_list()
+model = [device_name] + [device_name + '/' + attr for attr in attrs]
+w = TaurusForm()
+
+w.model = model
+w.show()
+sys.exit(app.exec_())
