ab7b36ce3fd6eceaa7370a9aa12e9273a942f5c2
Qing Zhang <guanggevwu@gmail.com>
2026-05-27 09:46:38 -0400

    Change back to python instead of pythonw because otherwise a terminal starts every time run another process from the GUI

--- shortcut/Windows/DAQ.bat
+++ shortcut/Windows/DAQ.bat
@@ -2,4 +2,4 @@ set "PATH_TWO_LEVELS_UP="
 for %%a in ("%~dp0..\..") do set "PATH_TWO_LEVELS_UP=%%~fa"
 cd %PATH_TWO_LEVELS_UP%
 git pull
-".\venv\Scripts\pythonw.exe" ".\DAQ\GUI.py"
+".\venv\Scripts\python.exe" ".\DAQ\GUI.py"
