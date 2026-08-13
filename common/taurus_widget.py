e86929dcee98c2ca250c7f795b4e3b722b3f02d6
Qing <guanggevwu@gmail.com>
2026-08-13 14:41:45 -0400

    Change Taurus LED widget  to clickable.

--- common/taurus_widget.py
+++ common/taurus_widget.py
@@ -14,15 +14,60 @@ class MyTaurusValueCheckBox(TaurusValueCheckBox):
         self.showText = False
 
 
+class ToggleTaurusLed(TaurusLed):
+    """A TaurusLed that toggles a writable boolean attribute on left click."""
+
+    def __init__(self, parent=None, designMode=False):
+        super().__init__(parent=parent, designMode=designMode)
+        self.setCursor(Qt.Qt.CursorShape.PointingHandCursor)
+        self.setToolTip("Click to toggle")
+
+    def mouseReleaseEvent(self, a0):
+        if a0 is not None and a0.button() == Qt.Qt.MouseButton.LeftButton and self._toggle_value():
+            a0.accept()
+            return
+        super().mouseReleaseEvent(a0)
+
+    def _toggle_value(self):
+        model = self.getModelObj()
+        if model is None:
+            return False
+        try:
+            value = model.read().rvalue
+            value = getattr(value, "magnitude", value)
+            model.write(not bool(value))
+            return True
+        except Exception as exc:
+            self.warning("Could not toggle %s: %s", self.getModelName(), exc)
+            return False
+
+
 class BoolLedSwitcher(TaurusReadWriteSwitcher):
-    """Shows a TaurusLed; double-click or F2 switches to a checkbox for editing.
+    """Shows a TaurusLed that toggles on click; F2 switches to checkbox editing.
     Use as the read widget of a TaurusForm row with write widget set to None:
         row.setReadWidgetClass(BoolLedSwitcher)
         row.setWriteWidgetClass(None)
     """
-    readWClass = TaurusLed
+    readWClass = ToggleTaurusLed
     writeWClass = MyTaurusValueCheckBox
-    exitEditTriggers = ("applied", Qt.Qt.Key_Escape)
+    enterEditTriggers = (Qt.Qt.Key.Key_F2,)
+    exitEditTriggers = ("applied", Qt.Qt.Key.Key_Escape)
+
+    def _onEnterEditActionTriggered(self):
+        self._sync_write_widget()
+        super()._onEnterEditActionTriggered()
+
+    def _sync_write_widget(self):
+        model = self.getModelObj()
+        if model is None or self.writeWidget is None:
+            return
+        try:
+            value = model.read().rvalue
+            value = getattr(value, "magnitude", value)
+            self.writeWidget.blockSignals(True)
+            self.writeWidget.setValue(value)
+        finally:
+            self.writeWidget.blockSignals(False)
 
 
 def add_value_pairs(values, autoApply=True):
